006caa64f6b0888a51bf69ccdf9517f6d8b7f160
HusseinYusufali <47536507+HusseinYusufali@users.noreply.github.com>
2022-08-27 16:10:01 +0100

    Initial Commit

---
+++ HappyTransformerPrediction.py
@@ -0,0 +1,5 @@
+from happytransformer import HappyWordPrediction
+from happytransformer import HappyGeneration
+
+Happy_wp_roberta = HappyWordPrediction("ROBERTA", "roberta-large")
+Happy_wp_roberta.save("model/")
